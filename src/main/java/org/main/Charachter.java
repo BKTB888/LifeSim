60e662d6303f1cc2d0304946b467cca0839d7b13
Bánfalvi-Kovács Tivadar Botond <BKTB888@icloud.com>
2024-10-24 18:30:39 +0200

    Replaced my own Name class with aj browns, and made it's tests use junit 5

--- src/main/java/org/main/Charachter.java
+++ src/main/java/org/main/Charachter.java
@@ -1,16 +1,30 @@
 package org.main;
 
+import org.ajbrown.namemachine.Gender;
+import org.ajbrown.namemachine.Name;
+import org.ajbrown.namemachine.NameGenerator;
+import org.ajbrown.namemachine.NameGeneratorOptions;
 import org.controller.Controller;
-import org.helper.Name;
 
 import java.util.List;
 
 public class Charachter {
+    static NameGeneratorOptions nameOptions = new NameGeneratorOptions();
+    static NameGenerator nameGen = new NameGenerator(nameOptions);
+    static {
+        nameOptions.setRandomSeed(42L);
+    }
+
     Name name;
+    Gender gender;
+
     Stats stats;
     List<Action> availableActions;
     Controller myController;
-    Boolean isMale;
 
     public List<Action> getActions(){return availableActions;}
+    public Charachter(){
+        this.name = nameGen.generateName();
+        this.gender = name.getGender();
+    }
 }
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,10 +1,10 @@
 package org.main;
 
+import org.ajbrown.namemachine.NameGeneratorOptions;
 import org.controller.AI;
 import org.controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
